d762b687062d9508e5433356a6f9a531b997da0f
Nikolay Krantsevich <krantsevich@gmail.com>
2021-11-12 16:41:20 -0700

    R: removed wsbcf object handling from getters

--- R/getters.R
+++ R/getters.R
@@ -1,33 +1,29 @@
 #' This function allows to get individual level treatment estimates.
 #'
-#' @param fit A wsbcf or XBCF fit object.
+#' @param fit An XBCF fit object.
 #'
 #' @return An array of treatment effect point estimates.
 #' @export
 getTaus <- function(fit) {
-    if(class(fit) != "XBCF" && class(fit) != "wsbcf")
-        stop("Can only get taus for an XBCF or WSBCF object.")
-    if(class(fit) == "XBCF")
+    if(class(fit) != "XBCF")
+        stop("Can only get taus for an XBCF object.")
+    else
         tauhats <- rowMeans(fit$tauhats.adjusted)
-    if(class(fit) == "wsbcf")
-        tauhats <- rowMeans(fit$tau_draws)
 
     return(tauhats)
 }
 
 #' This function allows to get individual level prognostic estimates.
 #'
-#' @param fit A wsbcf or XBCF fit object.
+#' @param fit An XBCF fit object.
 #'
 #' @return An array of prognostic effect point estimates.
 #' @export
 getMus <- function(fit) {
-    if(class(fit) != "XBCF" && class(fit) != "wsbcf")
-        stop("Can only get taus for an XBCF or WSBCF object.")
-    if(class(fit) == "XBCF")
+    if(class(fit) != "XBCF")
+        stop("Can only get taus for an XBCF object.")
+    else
         muhats <- rowMeans(fit$muhats.adjusted)
-    if(class(fit) == "wsbcf")
-        muhats <- rowMeans(fit$mu_draws)
 
     return(muhats)
 }
