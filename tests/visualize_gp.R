29224e3b042751e83ead3cda0f00aa45a8676793
MaggieWang0614 <maggiew614@gmail.com>
2022-01-10 19:58:31 -0700

    update test script

--- tests/visualize_gp.R
+++ tests/visualize_gp.R
@@ -3,8 +3,9 @@ library(XBCF)
 n = 500
 nt = 200
 x = as.matrix(rnorm(n+nt, 0, 5), n+nt,1)
-tau = 5 + cos(0.5*x +1)
-A = rbinom(n+nt, 1, 0*(abs(x)>5) + 0.5*(abs(x)<=5))
+# tau = 5 + cos(0.5*x +1)
+tau =  -0.1*x
+A = rbinom(n+nt, 1, 0*(x>5) + 0.5*(abs(x)<=5) + 1*(x< -5))
 # A = rbinom(n+nt, 1, 0*(x>5)+0.5*(x<=5))
 y1 = cos(0.2*x) + tau
 y0 = cos(0.2*x)
@@ -23,7 +24,10 @@ ytrain = as.matrix(y[1:n]); ytest = as.matrix(y[(n+1):(n+nt)])
 ztrain = as.matrix(A[1:n]); ztest = as.matrix(A[(n+1):(n+nt)])
 # pihat_tr = pihat[1:n]; pihat_te = pihat[(n+1):(n+nt)]
 xtrain = as.matrix(x[1:n,]); xtest = as.matrix(x[(n+1):(n+nt),])
+tautr = tau[1:n]; taute = tau[(n+1):(n+nt)]
 
+# test on train
+ytest = ytrain; xtest = xtrain; ztest = ztrain; taute = tautr
 # run XBCF
 t1 = proc.time()
 burnin = 20;
@@ -43,8 +47,7 @@ tauhats.pred <- rowMeans(pred$taudraws)
 tauhats.gp <- rowMeans(pred.gp$taudraws)
 
 # true tau?
-tau_te = tau[(n+1):(n+nt)]
-cat('True ATE:, ', round(mean(tau_te), 3), ', GP tau: ', round(mean(tauhats.gp), 3), 
+cat('True ATE:, ', round(mean(taute), 3), ', GP tau: ', round(mean(tauhats.gp), 3), 
     ', XBCF tau: ', round(mean(tauhats.pred), 3), '\n')
 
 gp.upper <- apply(pred.gp$taudraws, 1, quantile, 0.975, na.rm = TRUE)
@@ -54,18 +57,20 @@ xbcf.lower <- apply(pred$taudraws, 1, quantile, 0.025, na.rm = TRUE)
 
 # evaluate coverage
 cat('Coverage:', '\n')
-cat('GP = ', round(mean((gp.upper >= tau_te) & (gp.lower <= tau_te)), 3), '\n')
-cat('XBCF = ', round(mean((xbcf.upper >= tau_te) & (xbcf.lower <= tau_te)), 3), '\n')
+cat('GP = ', round(mean((gp.upper >= taute) & (gp.lower <= taute)), 3), '\n')
+cat('XBCF = ', round(mean((xbcf.upper >= taute) & (xbcf.lower <= taute)), 3), '\n')
 
 # plot(xtest, ytest, col = ztest+1)
 # readline()
-plot(xtest, tau_te, ylim = range(c(tau_te, tauhats.pred, tauhats.gp, gp.upper, gp.lower)))
-points(xtest, tauhats.gp, col = 2)
-points(xtest, tauhats.pred, col = 4) # the same ????
+plot(xtest, taute, ylim = range(c(taute, tauhats.pred, tauhats.gp, gp.upper, gp.lower)), col = A+1, cex = 0.5)
+points(xtest, tauhats.gp, col = 5, cex = 0.5)
+points(xtest, tauhats.pred, col = 4, cex = 0.5) # the same ????
+# readline()
+legend('bottomright', col = 1:5, legend = c('control', 'treated', 'gp bound', 'xbcf', 'gp'), lty = 1)
+
+points(xtest, gp.upper, col = 3, cex = 0.5)
+points(xtest, gp.lower, col = 3, cex = 0.5)
 # readline()
 
-points(xtest, gp.upper, col = 3)
-points(xtest, gp.lower, col = 3)
-# readline()
-
-# plot(xtest, gp.upper-gp.lower)
+# points(xtest, rowMeans(pred.gp$mudraws), col = 5)
+# points(xtest, ytest - rowMeans(pred.gp$mudraws), col = 5)
